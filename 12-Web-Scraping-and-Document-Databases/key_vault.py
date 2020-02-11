ed06a2bd3285bbb28aa47dcdc0f75cb80e0d4072
andres2520 <andrespaulding2520@gmail.com>
2020-02-10 20:47:56 -0500

    Web Scraping assignment

---
+++ 12-Web-Scraping-and-Document-Databases/key_vault.py
@@ -0,0 +1,5 @@
+#Visit --- https://www.digitalocean.com/community/tutorials/how-to-authenticate-a-python-application-with-twitter-using-tweepy-on-ubuntu-14-04
+consumer_key = 'enter'
+consumer_secret = 'enter'
+access_token = 'enter'
+access_token_secret = 'enter'
